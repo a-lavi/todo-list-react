36e317b8611151dae8580616cba90ef4305401b1
a-lavi <ali.rahmani.63@icloud.com>
2022-06-25 12:49:12 +0200

    Start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './Style.css';
+import List from './List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+
+        <div class="container text-muted">
+            <h2 id="head" class="display-3 text-center mb-5">To-do List</h2>
+            
+            <input class="form-control" id="new-task" type="text" placeholder="Add a new task" />
+            
+            <div id="tasks-body">
+                <h3 id="doing" class="text-center">My tasks</h3>
+                <ul id="todo-container">
+                  <List />
+                </ul>
+                <footer id='activeTodo'></footer>
+            </div>
+        </div>
+    </main>
   );
 }
 
