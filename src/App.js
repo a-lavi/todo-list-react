9e3b4f628bfc3c4a2d236e620b10d327a6763caf
Ling <abbylinglin@gmail.com>
2022-06-29 14:55:46 +0200

    update page design

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@
 import './Style.css';
 import List from './List';
 
-function App() {
-  return (
-    <main>
 
-        <div class="container text-muted">
-            <h2 id="head" class="display-3 text-center mb-5">To-do List</h2>
-            
-            <input class="form-control" id="new-task" type="text" placeholder="Add a new task" />
-            
-            <div id="tasks-body">
-                <h3 id="doing" class="text-center">My tasks</h3>
-                <ul id="todo-container">
-                  <List />
-                </ul>
-                <footer id='activeTodo'></footer>
+function App() {
+    return (
+        <main>
+
+            <div className="container">
+                <h1 id="head" >To-do List</h1>
+
+                <input className="form-control" id="new-task" type="text" placeholder="Add a new task" />
+                <center><button type='submit' className='add'>Add</button></center>
+
+                <div id="tasks-body">
+                    <h3 id="doing" className="text-center">My tasks</h3>
+                    <ul id="todo-container">
+                        <List />
+                    </ul>
+
+                </div>
             </div>
-        </div>
-    </main>
-  );
+        </main>
+    );
 }
 
 export default App;
+
+
--- src/List.js
+++ src/List.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const List = () => {
-  return (
-    <li className="todoItem">
-    <input type="checkbox" class="checkBox"/>
-    <label>TEst</label>
-    <input type="text" class="form-control"/>
-    <button className="delete"></button>
-    <button className="edit"></button>
-    <button className="down"></button>
-    <button className="up"></button>
-   </li>
-  )
+    return (
+        <li className="todoItem">
+            <input type="checkbox" class="checkBox" />
+            <label>TEST TASK</label>
+            <input type="text" class="form-control" />
+            <button className="delete"></button>
+            <button className="edit"></button>
+            <button className="down"></button>
+            <button className="up"></button>
+        </li>
+    )
 }
 
 export default List
