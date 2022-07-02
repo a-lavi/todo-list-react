72942c86c58af1d7c38397c101ab924660914b22
a-lavi <ali.rahmani.63@icloud.com>
2022-07-02 14:52:37 +0200

    add 'add' and 'remove'

--- src/App.js
+++ src/App.js
@@ -2,32 +2,29 @@
 import './Style.css';
 import ToDoList from './ToDoList';
 import Input from './Input';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import React, { useState } from 'react';
+
 
 
 function App() {
-  const myTasks = [
-    {
-      id:1,
-      description: 'Test',
-      isDone: true
-    },
-    {
-      id:2,
-      description: 'Test2',
-      isDone: false
+  
+  
+    const[newTask, setNewTask] = useState({task:'', status:''});
+    const [taskList , setTaskList]= useState([])
+    const addList = ()=>{
+      setTaskList(prev =>([...prev, newTask]));
     }
-  ]
+   // console.log(...taskList)
   return (
     <main>
 
         <div className="container ">
             <h2 id="head" >To-do List</h2>
-            <Input />
+            <Input setNewTask={setNewTask} addList={addList} newTask={newTask}/>
             <div id="task-body">
                 <h3 id="doing" className="text-center">My tasks</h3>
                 <ul id="todo-container">
-                  <ToDoList tasks={myTasks}/>
+                  <ToDoList taskList={taskList} setTaskList={setTaskList}/>
                 </ul>
                 <footer id='activeTodo'></footer>
             </div>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-const ToDoList = ({ tasks }) => {
-  const allTasks = tasks.map((task, index) => {
+const ToDoList = ({ taskList, setTaskList  }) => {
+  
+    const allTasks = taskList.map((list, index) => {
+      const removeTask = (props) => {
+        setTaskList(prev => prev.filter((index) => index !== props))
+      }
+      return (
+        <li className="todoItem" key={'mein' + index}>
+          <div>{list.task}</div>
+          <input type="checkbox" className="checkBox" />
+          <label></label>
+          <input type="text" className="form-control" />
+          <div>
+          <Button variant="contained" className="delete"
+           onClick={() => removeTask(index)} key={index} 
+           >{list}</Button>
+          </div>
+          <button className="edit"></button>
+          <button className="down"></button>
+          <button className="up"></button>
+        </li>
+      )
+
+    })
     return (
-      <li className="todoItem" key = {'mein'+task.id}>
-        <div></div>
-        <input type="checkbox" className="checkBox" />
-        <label>{task.description}</label>
-        <input type="text" className="form-control" />
-        <Button variant="contained" className="delete"></Button>
-        <button className="edit"></button>
-        <button className="down"></button>
-        <button className="up"></button>
-      </li>
+      <>
+        {allTasks}
+      </>
     )
+  }
 
-  })
-  return (
-    <>
-      {allTasks}
-    </>
-  )
-}
-
-export default ToDoList
+  export default ToDoList
