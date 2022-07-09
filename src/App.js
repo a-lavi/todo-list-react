7fd59f5c6c6913e0882cd5da46ec8025441fc759
Ling <abbylinglin@gmail.com>
2022-07-09 09:39:04 +0200

    update css and Popup Component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import './Style.css';
-import ToDoList from './ToDoList';
-import Input from './Input';
-import React, { useState } from 'react';
-
-
+import './Style.css'
+import ToDoList from './ToDoList'
+import Input from './Input'
+import React, { useState } from 'react'
 
 function App() {
+    const [newTask, setNewTask] = useState({
+        task: '',
+        status: '',
+        isEditing: false,
+        completed: false,
+    })
+    const [taskList, setTaskList] = useState([])
+    //const [editing, setEditing]= useState(false)
 
-  const [newTask, setNewTask] = useState({ task: '', status: '',isEditing:false, completed:false });
-  const [taskList, setTaskList] = useState([])
-  //const [editing, setEditing]= useState(false)
-
-  const addList = () => {
-    if(!newTask.task){
-      console.log('hhhhhhhhh')
-    }else if(taskList.map(element => element.task).indexOf(newTask.task) !== -1){
-      console.log('change')
-    }else{
-      setTaskList(prev => ([...prev, newTask]));
-      
+    const addList = () => {
+        if (!newTask.task) {
+            console.log('hhhhhhhhh')
+        } else if (
+            taskList.map((element) => element.task).indexOf(newTask.task) !== -1
+        ) {
+            console.log('change')
+        } else {
+            setTaskList((prev) => [...prev, newTask])
+        }
     }
-    
-  }
- 
-  // console.log(...taskList)
-  return (
-    <main>
 
-      <div className="container ">
-        <h2 id="head" >To-do List</h2>
-        <Input setNewTask={setNewTask} addList={addList} newTask={newTask} taskList={taskList}  />
-        <div id="task-body">
-          <h3 id="doing" className="text-center">My tasks</h3>
-          <ul id="todo-container">
-            <ToDoList  taskList={taskList} setTaskList={setTaskList} setNewTask={setNewTask} addList={addList} newTask={newTask} />
-          </ul>
-          <footer id='activeTodo'></footer>
-        </div>
-      </div>
-    </main>
-  );
-  
+    // console.log(...taskList)
+    return (
+        <main>
+            <div className="container ">
+                <h2 id="head">To-do List</h2>
+                <Input
+                    setNewTask={setNewTask}
+                    addList={addList}
+                    newTask={newTask}
+                    taskList={taskList}
+                />
+                <div id="task-body">
+                    <h3 id="doing" className="text-center">
+                        My tasks
+                    </h3>
+                    <ul id="todo-container">
+                        <ToDoList
+                            taskList={taskList}
+                            setTaskList={setTaskList}
+                            setNewTask={setNewTask}
+                            addList={addList}
+                            newTask={newTask}
+                        />
+                    </ul>
+                </div>
+            </div>
+        </main>
+    )
 }
-export default App;
+export default App
--- src/toDoListItem.js
+++ src/toDoListItem.js
@@ -1,47 +1,65 @@
-import Button from '@mui/material/Button';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
-import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
-import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined';
-import React, { useState } from 'react';
-const ToDoListItem = ({ list, index, taskList, setTaskList, setNewTask, addList, newTask, removeTask }) => {
-
+import Button from '@mui/material/Button'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined'
+import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined'
+import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined'
+import React, { useState } from 'react'
+const ToDoListItem = ({
+    list,
+    index,
+    taskList,
+    setTaskList,
+    setNewTask,
+    addList,
+    newTask,
+    removeTask,
+    id,
+}) => {
     const [editing, setEditing] = useState(false)
     const [updateTask, setUpdateTask] = useState([])
     const [toggle, setToggle] = useState(false)
+    const [popup, setPopup] = useState({
+        show: false, // initial values set to false and null
+        id: null,
+    })
 
     const toggleComplete = () => {
-        
-
         setToggle(!toggle)
-
     }
     const renderName = ({ list }) => {
         console.log('test!')
         const itemStyle = {
             'text-decoration': toggle ? 'line-through' : 'none',
-            cursor: 'pointer'
-        };
+            cursor: 'pointer',
+        }
 
         if (editing) {
             console.log('i am edit')
             return (
                 <form onSubmit={onSaveClick}>
-                    <input className='form-control' type="text" id='update-task' value={updateTask.task} onChange={({ target }) => setUpdateTask(prev => ({ ...prev, task: target.value }))} />
+                    <input
+                        className="form-control"
+                        type="text"
+                        id="update-task"
+                        value={updateTask.task}
+                        onChange={({ target }) =>
+                            setUpdateTask((prev) => ({
+                                ...prev,
+                                task: target.value,
+                            }))
+                        }
+                    />
                 </form>
-
-            );
+            )
         } else {
             console.log('test2', editing)
             return (
                 <>
-                    <span style={itemStyle} onClick={toggleComplete}>{list.task}</span>
-                    <input type="checkbox"
-                        //onClick={this.props.toggleComplete.bind(this, this.props.name)} 
-                        className="checkBox" />
-                    <label>sdd</label>
+                    <span style={itemStyle} onClick={toggleComplete}>
+                        {list.task}
+                    </span>
                 </>
-            );
+            )
         }
     }
     const renderButtons = ({ list, index }) => {
@@ -49,64 +67,89 @@ const ToDoListItem = ({ list, index, taskList, setTaskList, setNewTask, addList,
             return (
                 <span>
                     <button onClick={onSaveClick}>Save</button>
-                    <button onClick={onCancel} >Cancel</button>
+                    <button onClick={onCancel}>Cancel</button>
                 </span>
-            );
+            )
         } else {
-
             return (
                 <span>
-
                     <div>
-                        <Button className="delete"
-                            onClick={() => removeTask(index)}
-                        ><CloseOutlinedIcon /></Button>
+                        <Button
+                            className="delete"
+                            onClick={() => handleDelete(id)}>
+                            <CloseOutlinedIcon />
+                        </Button>
                     </div>
-                    <Button onClick={() => onEditClick(list)} className="edit"><CreateOutlinedIcon /></Button>
-                    <Button className="down"><ArrowDownwardOutlinedIcon /></Button>
-                    <Button className="up"><ArrowUpwardOutlinedIcon /></Button>
+                    <Button onClick={() => onEditClick(list)} className="edit">
+                        <CreateOutlinedIcon />
+                    </Button>
+                    <Button className="down">
+                        <ArrowDownwardOutlinedIcon />
+                    </Button>
+                    <Button className="up">
+                        <ArrowUpwardOutlinedIcon />
+                    </Button>
                 </span>
-            );
+            )
         }
-
     }
-    const findItem = (item) => {
-        return taskList.filter((element) => element.task === item)[0];
 
+    // This shows the Confirmation Box
+
+    const handleDelete = (id) => {
+        setPopup({
+            show: true,
+            id,
+        })
+    }
+
+    // This performs the deletion and hide the Confirmation Box
+
+    const handleDeleteTrue = () => {
+        if (popup.show && popup.id) {
+            let filteredData = updateTask.filter((todo) => todo.id !== popup.id)
+            setUpdateTask(filteredData)
+            setPopup({
+                show: false,
+                id: null,
+            })
+        }
+    }
+
+    // This hides the Confirmation Box when user clicks "No"/"Cancel"
+
+    const handleDeleteFalse = () => {
+        setPopup({
+            show: false,
+            id: null,
+        })
+    }
+
+    const findItem = (item) => {
+        return taskList.filter((element) => element.task === item)[0]
     }
     const onSaveClick = () => {
-
-
-        saveItem(list, updateTask.task);
+        saveItem(list, updateTask.task)
         setEditing(false)
     }
     const saveItem = (oldItem, newItem) => {
-        let selectedItem = findItem(oldItem.task);
-        selectedItem.task = newItem;
+        let selectedItem = findItem(oldItem.task)
+        selectedItem.task = newItem
         //setTaskList(prev => ([...prev, selectedItem]));
     }
     const onEditClick = () => {
-
         setEditing(true)
-        renderName({ list });
-
+        renderName({ list })
     }
     const onCancel = () => {
         setEditing(false)
     }
 
-
     return (
         <div className="to-do-item">
-            <span className="name">
-                {renderName({ list })}
-            </span>
-            <span className="actions">
-                {renderButtons({ list, index })}
-            </span>
+            <span className="name">{renderName({ list })}</span>
+            <span className="actions">{renderButtons({ list, index })}</span>
         </div>
-    );
-
-
+    )
 }
 export default ToDoListItem
