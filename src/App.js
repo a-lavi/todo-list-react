8547bdc7d783524bd280c82383772d359ed9fb93
Ling <abbylinglin@gmail.com>
2022-07-09 16:54:45 +0200

    update style&clean up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Input from './Input'
 import React, { useState } from 'react'
 
 function App() {
-  
     const [newTask, setNewTask] = useState({
         task: '',
         status: '',
@@ -12,27 +11,24 @@ function App() {
         completed: false,
     })
     const [taskList, setTaskList] = useState([])
-    const [errorMsg , setErroMsg] = useState('')
+    const [errorMsg, setErroMsg] = useState('')
     //const [editing, setEditing]= useState(false)
 
     const addList = () => {
         if (!newTask.task) {
-          
-          setErroMsg('Please enter a valid Task!!!')
-          setNewTask((prev)=>({...prev, task:''}))
-        
+            setErroMsg('Please enter a valid task.')
+            setNewTask((prev) => ({ ...prev, task: '' }))
         } else if (
             taskList.map((element) => element.task).indexOf(newTask.task) !== -1
         ) {
             console.log('change')
-            setNewTask((prev)=>({...prev, task:''}))
-            setErroMsg('There is already a task with this name!!!')
+            setNewTask((prev) => ({ ...prev, task: '' }))
+            setErroMsg('This task already exists, please add a new one.')
         } else {
-          setErroMsg('')
-            setTaskList((prev) => [...prev, {...newTask}])
-            setNewTask((prev)=>({...prev, task:''}))
+            setErroMsg('')
+            setTaskList((prev) => [...prev, { ...newTask }])
+            setNewTask((prev) => ({ ...prev, task: '' }))
         }
-       
     }
 
     // console.log(...taskList)
@@ -41,14 +37,14 @@ function App() {
             <div className="container ">
                 <h2 id="head">To-do List</h2>
                 <Input
-                errorMsg={errorMsg}
-                setErroMsg={setErroMsg}
+                    errorMsg={errorMsg}
+                    setErroMsg={setErroMsg}
                     setNewTask={setNewTask}
                     addList={addList}
                     newTask={newTask}
                     taskList={taskList}
                 />
-                
+
                 <div id="task-body">
                     <h3 id="doing" className="text-center">
                         My tasks
